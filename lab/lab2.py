2c32288148cb310959fdc29155690a22f9c70077
sweetienianyu <475493955@qq.com>
2020-05-14 16:39:50 +0800

    update lab2

--- lab/lab2.py
+++ lab/lab2.py
@@ -1,4 +1,8 @@
+# link https://cs61a.org/lab/sol-lab02/#lambda-expressions
 
+from operator import add, mul, mod
+
+# Q3: Lambdas and Currying
 def lambda_curry2(func):
     """
     Returns a Curried version of a two-argument function FUNC.
@@ -14,4 +18,112 @@ def lambda_curry2(func):
     >>> lambda_curry2(mod)(123)(10)
     3
     """
-    return lambda arg1: lambda arg2: func(arg1, arg2)
+    
+    # def step1(x):
+    #     def step2(y):
+    #         return func(x, y)
+    #     return step2
+    # return step1
+    # standard anwser: return lambda arg1: lambda arg2: func(arg1, arg2)
+    return lambda x : lambda y: func(x, y)
+
+
+# Q4: Count van Count
+def count_factors(n):
+    """Return the number of positive factors that n has.
+    >>> count_factors(6)
+    4   # 1, 2, 3, 6
+    >>> count_factors(4)
+    3   # 1, 2, 4
+    """
+    i = 1
+    count = 0
+    while i <= n:
+        if n % i == 0:
+            count = count + 1
+        i = i + 1
+    return count
+
+def is_prime(n):
+    return count_factors(n) == 2
+    
+def count_primes(n):
+    """Return the number of prime numbers up to and including n.
+    >>> count_primes(6)
+    3   # 2, 3, 5
+    >>> count_primes(13)
+    6   # 2, 3, 5, 7, 11, 13
+    """
+    i = 1
+    count = 0
+    while i <= n:
+        if is_prime(i):
+            count = count + 1
+        i = i + 1
+    return count
+
+def count_cond(condition):
+    """Returns a function with one parameter N that counts all the numbers from
+    1 to N that satisfy the two-argument predicate function Condition, where
+    the first argument for Condition is N and the second argument is the
+    number from 1 to N.
+
+    >>> count_factors = count_cond(lambda n, i: n % i == 0)
+    >>> count_factors(2)   # 1, 2
+    2
+    >>> count_factors(4)   # 1, 2, 4
+    3
+    >>> count_factors(12)  # 1, 2, 3, 4, 6, 12
+    6
+
+    >>> is_prime = lambda n, i: count_factors(i) == 2
+    >>> count_primes = count_cond(is_prime)
+    >>> count_primes(2)    # 2
+    1
+    >>> count_primes(3)    # 2, 3
+    2
+    >>> count_primes(4)    # 2, 3
+    2
+    >>> count_primes(5)    # 2, 3, 5
+    3
+    >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
+    8
+    """
+
+    def inner(n):
+        i = 1
+        count = 0
+        while i <= n:
+            if condition(n, i):
+                count = count + 1
+            i = i + 1
+        return count
+    return inner
+    
+
+
+# Q5  Both Paths
+# s:start 
+# l:left 
+# r:right
+
+def both_paths(sofar="S"):
+    """
+    >>> left, right = both_paths()
+    S
+    >>> leftleft, leftright = left()
+    SL
+    >>> rightleft, rightright = right()
+    SR
+    >>> _ = leftleft()
+    SLL
+    """
+    print(sofar)
+    def left():
+        return both_paths(sofar + "L")
+    def right():
+        return both_paths(sofar + "R")
+    return left, right
+
+
+
